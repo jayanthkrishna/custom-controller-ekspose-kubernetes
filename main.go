d981ab6b2afed4f983f3749866378fdbe06452a7
jayanthkrishna <jayanth.krishna.g@gmail.com>
2023-08-07 23:36:16 +0530

    Contoller skeleton code added

--- main.go
+++ main.go
@@ -3,32 +3,49 @@ package main
 import (
 	"flag"
 	"fmt"
+	"path/filepath"
+	"time"
 
+	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 )
 
 func main() {
 
-	kubeconfig := flag.String("kubeconfig", "/home/eangrki/.kube/config", "location to your config file")
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "location to kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
 
+	fmt.Println(*kubeconfig)
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-
 	if err != nil {
-		fmt.Printf("error %s building from flags", err.Error())
-
-		config, err = rest.InClusterConfig()
-
-		if err != nil {
-			fmt.Printf("error %s from InClusterConfig", err.Error())
-		}
+		panic(err.Error())
 	}
+
+	// create the clientset
 	clientSet, err := kubernetes.NewForConfig(config)
-
 	if err != nil {
-		fmt.Printf("error %s building the client", err.Error())
+		panic(err.Error())
 	}
 
-	fmt.Println(clientSet)
+	// fmt.Println(clientSet)
+
+	informerFactory := informers.NewSharedInformerFactory(clientSet, 10*time.Second)
+
+	ch := make(chan struct{})
+
+	c := newController(clientSet, informerFactory.Apps().V1().Deployments())
+
+	c.run(ch)
+
+	informerFactory.Start(ch)
+
+	fmt.Println(informerFactory)
+
 }
